4d5c7814af077f8a0109792c0a582725e705a8d3
whisparo <140510844+whisparo@users.noreply.github.com>
2026-07-09 21:12:25 +0300

    fix: force cloudflare wrangler configuration build

--- open-next.config.ts
+++ open-next.config.ts
@@ -3,7 +3,7 @@ import type { OpenNextConfig } from "@opennextjs/cloudflare";
 const config: OpenNextConfig = {
   default: {
     override: {
-      wrapper: "cloudflare-node",
+      wrapper: "cloudflare-edge", // ✅ نغير لـ cloudflare-edge
       converter: "edge",
       proxyExternalRequest: "fetch",
       incrementalCache: "dummy",
